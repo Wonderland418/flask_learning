edf5ce3b488d5ef8fd6ff70552e5046ecab6c449
Wonderland418 <helpme7799332@gmail.com>
2025-05-02 21:24:46 +0800

    使用Template Inheritance

--- followMegaTutorial/microblog2/app/routes.py
+++ followMegaTutorial/microblog2/app/routes.py
@@ -1,8 +1,10 @@
+#基本上route是在處理：當使用者開啟一個網址時，Flask要回應什麼東西給他
 from flask import render_template
 from app import app
-
 @app.route('/')
+#使用者打開index這頁時
 @app.route('/index')
+#flask做的事
 def index():
     user = {'username':'Irene'}
     posts = [
@@ -15,4 +17,5 @@ def index():
             'body':'The Avengers movie was so cool!'
         }
     ]
+    #注意這裡寫的是給"index.html"
     return render_template('index.html', title = 'Home', user = user, posts = posts)
