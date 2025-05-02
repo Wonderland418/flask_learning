c7257d19a83ea824f380d3f0003199655221230a
Wonderland418 <helpme7799332@gmail.com>
2025-05-02 18:58:39 +0800

    在index.html中使用for迴圈顯示routes.py中post的內容

--- followMegaTutorial/microblog2/app/routes.py
+++ followMegaTutorial/microblog2/app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username':'Irene'}
-    return render_template('index.html', title = 'Home', user = user)
+    posts = [
+        {
+            'author':{'username':'John'},
+            'body':'Beautiful day in Poland!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
